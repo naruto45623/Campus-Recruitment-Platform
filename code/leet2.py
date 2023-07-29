0564c28d0392c9763bd33c04bda24e50ccea61e3
naruto45623 <karthikbaka222@gmail.com>
2023-07-29 14:23:08 +0530

    first commit

--- code/leet2.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://leetcode-stats-api.herokuapp.com/dharmikbanka23"
-response = requests.get(url)
-data = response.json()
-
-total_solved = data['totalSolved']
-
-print(total_solved)
